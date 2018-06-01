11ba1417f54aab3653e24a4c0d90871679755603
Nick Leli <nleli@newrelic.com>
2018-06-01 09:22:29 -0700

    WIP before discovery response

--- main.go
+++ main.go
@@ -1,189 +1,50 @@
 package main
 
 import (
+	"context"
+	"io"
+	"log"
 	"net"
-	"sync"
-	"sync/atomic"
-	"time"
 
 	"github.com/envoyproxy/go-control-plane/envoy/api/v2"
-	"github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
-	"github.com/envoyproxy/go-control-plane/envoy/api/v2/listener"
-	"github.com/envoyproxy/go-control-plane/envoy/api/v2/route"
-	hcm "github.com/envoyproxy/go-control-plane/envoy/config/filter/network/http_connection_manager/v2"
-	"github.com/envoyproxy/go-control-plane/pkg/cache"
-	"github.com/envoyproxy/go-control-plane/pkg/server"
-	"github.com/envoyproxy/go-control-plane/pkg/test"
-	"github.com/envoyproxy/go-control-plane/pkg/util"
-
-	log "github.com/sirupsen/logrus"
-	grpc "google.golang.org/grpc"
+	"google.golang.org/grpc"
 )
 
-var (
-	localhost = "127.0.0.1"
-	version   int32
+const (
+	proto   = "tcp"
+	iface   = "localhost"
+	mgrPort = "19000"
 )
+
+type listenerServer struct {
+}
 
 func main() {
-	// create a cache
-	signal := make(chan struct{})
-	cb := &callbacks{signal: signal}
-	config := cache.NewSnapshotCache(false, test.Hasher{}, logger{})
-	srv := server.NewServer(config, cb)
+	lis, err := net.Listen(proto, iface+":"+mgrPort)
+	if err != nil {
+		log.Fatalf("Error getting listener: %v", err)
+	}
+	// var srv v2.ListenerDiscoveryServiceServer
 	grpc := grpc.NewServer()
-	v2.RegisterListenerDiscoveryServiceServer(grpc, srv)
-	v2.RegisterClusterDiscoveryServiceServer(grpc, srv)
-	lis, err := net.Listen("tcp", "localhost:19000")
-	if err != nil {
-		log.Printf("Error acquiring listner: %v", err)
-	}
-	atomic.AddInt32(&version, 1)
-
-	var listenerName = "nick-xds"
-	var targetHost = "www.bbc.com"
-	var targetPrefix = "/robots.txt"
-	var virtualHostName = "local_service"
-	var remoteHost = "www.bbc.com"
-
-	var routeConfigName = "local_route"
-	nodeID := "test-id"
-	clusterName := "nick-xds"
-	h := &core.Address{Address: &core.Address_SocketAddress{
-		SocketAddress: &core.SocketAddress{
-			Address:  remoteHost,
-			Protocol: core.TCP,
-			PortSpecifier: &core.SocketAddress_PortValue{
-				PortValue: uint32(443),
-			},
-		},
-	}}
-	c := []cache.Resource{
-		&v2.Cluster{
-			Name:            clusterName,
-			ConnectTimeout:  2 * time.Second,
-			Type:            v2.Cluster_LOGICAL_DNS,
-			DnsLookupFamily: v2.Cluster_V4_ONLY,
-			LbPolicy:        v2.Cluster_ROUND_ROBIN,
-			Hosts:           []*core.Address{h},
-		},
-	}
-
-	v := route.VirtualHost{
-		Name:    virtualHostName,
-		Domains: []string{"*"},
-
-		Routes: []route.Route{{
-			Match: route.RouteMatch{
-				PathSpecifier: &route.RouteMatch_Prefix{
-					Prefix: targetPrefix,
-				},
-			},
-			Action: &route.Route_Route{
-				Route: &route.RouteAction{
-					HostRewriteSpecifier: &route.RouteAction_HostRewrite{
-						HostRewrite: targetHost,
-					},
-					ClusterSpecifier: &route.RouteAction_Cluster{
-						Cluster: clusterName,
-					},
-				},
-			},
-		}}}
-	manager := &hcm.HttpConnectionManager{
-		CodecType:  hcm.AUTO,
-		StatPrefix: "ingress_http",
-		RouteSpecifier: &hcm.HttpConnectionManager_RouteConfig{
-			RouteConfig: &v2.RouteConfiguration{
-				Name:         routeConfigName,
-				VirtualHosts: []route.VirtualHost{v},
-			},
-		},
-		HttpFilters: []*hcm.HttpFilter{{
-			Name: util.Router,
-		}},
-	}
-	pbst, err := util.MessageToStruct(manager)
-	if err != nil {
-		panic(err)
-	}
-
-	var l = []cache.Resource{
-		&v2.Listener{
-			Name: listenerName,
-			Address: core.Address{
-				Address: &core.Address_SocketAddress{
-					SocketAddress: &core.SocketAddress{
-						Protocol: core.TCP,
-						Address:  localhost,
-						PortSpecifier: &core.SocketAddress_PortValue{
-							PortValue: 80,
-						},
-					},
-				},
-			},
-			FilterChains: []listener.FilterChain{{
-				Filters: []listener.Filter{{
-					Name:   util.HTTPConnectionManager,
-					Config: pbst,
-				}},
-			}},
-		}}
-	log.Infof(">>>>>>>>>>>>>>>>>>> creating snapshot Version 0")
-	snap := cache.NewSnapshot(string(version), nil, c, nil, l)
-
-	config.SetSnapshot(nodeID, snap)
+	v2.RegisterListenerDiscoveryServiceServer(grpc, &listenerServer{})
+	log.Printf("Listening on: %v", "http://"+iface+":"+mgrPort)
 	grpc.Serve(lis)
 }
 
-type logger struct{}
-
-func (logger logger) Infof(format string, args ...interface{}) {
-	log.Debugf(format, args...)
-}
-func (logger logger) Errorf(format string, args ...interface{}) {
-	log.Errorf(format, args...)
-}
-
-// Hasher returns node ID as an ID
-type Hasher struct {
-}
-
-type callbacks struct {
-	signal   chan struct{}
-	fetches  int
-	requests int
-	mu       sync.Mutex
-}
-
-func (cb *callbacks) Report() {
-	cb.mu.Lock()
-	defer cb.mu.Unlock()
-	log.WithFields(log.Fields{"fetches": cb.fetches, "requests": cb.requests}).Info("server callbacks")
-}
-func (cb *callbacks) OnStreamOpen(id int64, typ string) {
-	log.Debugf("stream %d open for %s", id, typ)
-}
-func (cb *callbacks) OnStreamClosed(id int64) {
-	log.Debugf("stream %d closed", id)
-}
-func (cb *callbacks) OnStreamRequest(int64, *v2.DiscoveryRequest) {
-	cb.mu.Lock()
-	defer cb.mu.Unlock()
-	cb.requests++
-	if cb.signal != nil {
-		close(cb.signal)
-		cb.signal = nil
+func (l *listenerServer) StreamListeners(sls v2.ListenerDiscoveryService_StreamListenersServer) error {
+	log.Print("In the stream")
+	for {
+		dr, err := sls.Recv()
+		log.Print(dr)
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
 	}
 }
-func (cb *callbacks) OnStreamResponse(int64, *v2.DiscoveryRequest, *v2.DiscoveryResponse) {}
-func (cb *callbacks) OnFetchRequest(req *v2.DiscoveryRequest) {
-	cb.mu.Lock()
-	defer cb.mu.Unlock()
-	cb.fetches++
-	if cb.signal != nil {
-		close(cb.signal)
-		cb.signal = nil
-	}
+
+func (l *listenerServer) FetchListeners(context.Context, *v2.DiscoveryRequest) (*v2.DiscoveryResponse, error) {
+	return nil, nil
 }
-func (cb *callbacks) OnFetchResponse(*v2.DiscoveryRequest, *v2.DiscoveryResponse) {}
